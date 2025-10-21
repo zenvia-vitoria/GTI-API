509ab7e9f967ed35242c8baf227073171dbacaaa
Andre Souza <ajjunior33@gmail.com>
2025-10-21 17:58:28 -0300

    update

--- src/api/contexts/ticket/ticket.controller.ts
+++ src/api/contexts/ticket/ticket.controller.ts
@@ -1,31 +1,28 @@
-import {
-  NextFunction,
-  Request,
-  Response
-} from 'express';
-import { ITicketZCCService } from './interface/ticket.interface';
+import { NextFunction, Request, Response } from "express";
+import { ITicketZCCService } from "./interface/ticket.interface";
 
 export class TicketController {
-  constructor(private readonly ticketZCCService: ITicketZCCService) { }
+  constructor(private readonly ticketZCCService: ITicketZCCService) {}
 
   public async create(
     request: Request,
     response: Response,
-    next: NextFunction
+    next: NextFunction,
   ) {
     try {
       const data = request.body;
+
+      console.log(request.body);
+
       const handler = await this.ticketZCCService.create(data);
 
-      return response.status(200)
-        .json({
-          message: "Ticket criado com sucesso.",
-          status: true,
-          data: handler
-        })
-
+      return response.status(200).json({
+        message: "Ticket criado com sucesso.",
+        status: true,
+        data: handler,
+      });
     } catch (err) {
-      return next(err)
+      return next(err);
     }
   }
 }
